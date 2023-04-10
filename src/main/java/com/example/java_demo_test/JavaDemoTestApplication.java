7c5437e1b5b20e4892eb6fd542b26c6e72cea5b8
ZHEN <belong1201@gmail.com>
2023-04-10 10:28:13 +0800

    first commit

---
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_demo_test/JavaDemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_demo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
