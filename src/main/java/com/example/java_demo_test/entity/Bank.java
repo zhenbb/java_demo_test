0ee02055421b5f8be561f645157eaa54c36866fc
ZHEN <belong1201@gmail.com>
2023-04-11 11:38:00 +0800

    add Bank entity

---
+++ src/main/java/com/example/java_demo_test/entity/Bank.java
@@ -0,0 +1,23 @@
+package com.example.java_demo_test.entity;
+
+public class Bank {
+	private int account;
+	private int balance;
+	
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	
+	
+	
+}
